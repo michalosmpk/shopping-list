a981c99200e6c8fc5f63821faa77679219fdb242
michalosmpk <michalosmpk@gmail.com>
2022-09-03 05:19:04 +0200

    basic version

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php 
+	require_once('db.php');
+	if(isset($_POST['new-listing-button']) && !empty($_POST['new-listing-input'])) {
+		$conn->query('INSERT INTO list (listing_name) VALUES (\''.$_POST['new-listing-input'].'\')');
+		unset($_POST);
+		header('Location: index.php');
+	}
+	if(isset($_POST['delete-listing-button'])) {
+		echo $_POST['delete-listing-button'];
+		$conn->query('DELETE FROM list WHERE list.listing_id = '.$_POST['delete-listing-button']);
+		unset($_POST);
+		header('Location: index.php');
+	}
+
+?>
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,7 +40,31 @@
 			</span>
 		</button>
 	</header>
-
+	<content>
+		<form class="new-listing" method="post">
+			<input type="text" name="new-listing-input" id="" class="" maxlength="255">
+			<button type="submit" name="new-listing-button">
+				<span class="new-listing__plus"></span>
+			</button>
+		</form>
+		<form class="shopping-list" method="post">
+			<?php 
+				$result = $conn->query('select * from list;')->fetch_all(); 
+				foreach( $result as $row) {
+					?>
+					<div class="shopping-list__listing">
+						<p>
+							<?php echo $row[1]; ?>
+						</p>
+						<button type="submit" name="delete-listing-button" value="<?php echo $row[0];?>">
+							<span class="delete-listing-button__minus"></span>
+						</button>
+					</div>
+					<?php
+				}
+			?>
+		</form>
+	</content>
 	<script src="assets/js/main.js"></script>
 </body>
 </html>
