82b5070aeeb610208ea83db6757c439b83c2e666
michalosmpk <michalosmpk@gmail.com>
2022-09-20 20:22:44 +0200

    db sql

--- db.sql
+++ db.sql
@@ -1,2 +1,9 @@
-create table users( user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, username VARCHAR(255), password VARCHAR(255) );
-create table user_lists(relation_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, user_id INT, list_id INT);
+create database `shopping_list-basic`
+use `shopping_list-basic`
+CREATE TABLE `list` (
+  `listing_id` int NOT NULL AUTO_INCREMENT,
+  `listing_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`listing_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+-- create table users( user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, username VARCHAR(255), password VARCHAR(255) );
+-- create table user_lists(relation_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, user_id INT, list_id INT);
